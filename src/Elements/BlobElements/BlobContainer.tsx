56bbf48ee62317d4c925ea5f4bbc593a21acf99d
Nicholas Maselli <maselli.nicholas@gmail.com>
2021-11-23 23:35:37 -0500

    response

--- src/Elements/BlobElements/BlobContainer.tsx
+++ src/Elements/BlobElements/BlobContainer.tsx
@@ -2,7 +2,7 @@ import { BlobImage } from "./BlobImage";
 
 export const BlobContainer = () => 
 {
-    const test = [0,1,2,3,4,5,6,7,8,9];
+    const test = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20];
     //const colCount = "row-cols-5";
     return (
         <div className="blob-container">
--- src/Elements/TitleContainer/TitleContainer.tsx
+++ src/Elements/TitleContainer/TitleContainer.tsx
@@ -5,27 +5,11 @@ export const TitleContainer = () =>
             <div className="title-row row align-items-center">
                 <div className="col-1"></div>
                 <div className="col-8">
-                    <div className="row">
-                        <div className="col-10">
-                            <h1>Welcome To</h1>
-                            <h1>ADA Blobs!</h1>
-                        </div>
-                        <div className="col-2">
-                            <img width={256} height={256} src="images/Bob.png" />
-                        </div>
-                    </div>
+                    <h1>Start Your Blob</h1>
+                    <h1>Collection!</h1>
                     <p>
                         Launched on Octorber 31st, 2021, ADA Blobs is one of the first NFT projects on the Cardano Blockchain. There will only ever be 300 ADA Blobs available,
-                        with 1 new Blob being available each week until all 300 are revealed. ADA Blobs is built entirely on the Cardano blockchain using a decentralized
-                        smart contract to manage everything from buying and selling blobs, the price of Blobs, and the owner.
-                    </p>
-                    <p>
-                        The Blob characters originate from the Youtube channel Avatar Nick where they are used as characters in the various videos and games. 
-                        These Blobs are COMPLETELY USELESS beyond being bought and sold, and giving you a strong sense of pride in being the owner of 1 of
-                        only 300 Blobs.
-                    </p>
-                    <p>
-                        You can join this Discord Server to discuss the Blobs as well as find people who are buying / selling Blobs.
+                        with 1 new Blob being available each week until all 300 are revealed. 
                     </p>
                     <br />
                 </div>        
@@ -35,10 +19,10 @@ export const TitleContainer = () =>
             <style jsx>{`
                 .title-container {
                     background-image: linear-gradient(180deg, #143f6d, #1e5692);/*linear-gradient(180deg, #143f6d, #1e5692); linear-gradient(180deg, #2CBBE7, #2ea2be);*/
-                    height: 40rem;
+                    min-height: 10rem;
 
                     color: white;
-                    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
                 }
 
                 .title-row {
