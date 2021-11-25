315adc3e622da8bda91dc2f14b78b4318b6616c8
Nicholas Maselli <maselli.nicholas@gmail.com>
2021-11-24 23:01:10 -0500

    proper images

--- src/Elements/TitleContainer/TitleContainer.tsx
+++ src/Elements/TitleContainer/TitleContainer.tsx
@@ -6,20 +6,25 @@ export const TitleContainer = () =>
         <div className="title-container d-flex align-items-center">            
             <div className="container">
                 <div className="row">
+                </div>
+                <div className="row">
                     <div className="col-8">
                         <h1>Start Your Blob</h1>
                         <h1>Collection!</h1>
                         <p>
-                            Launched on Octorber 31st, 2021, ADA Blobs is one of the first NFT projects on the Cardano Blockchain. There will only ever be 300 ADA Blobs available,
-                            with 1 new Blob being available each week until all 300 are revealed. 
+                            Launched on November 30th, 2021, ADA Blobs is a collection of 300 Blob NFTs and is one of the first NFT projects on the Cardano Blockchain. There will only ever be 300 ADA Blobs available,
+                            with 1 new adorable and memeable Blob being available each week until all 300 are revealed. 
                         </p>
                     </div>
                     <div className="col-2">
-                        <div>
+                        <div className="d-none d-lg-block">
                             <img className="first-image-width" src="/images/blobs/001 - Bob.png" height={"125%"} width={"125%"} />                        
-                        </div>                        
+                        </div>
+                        <div className="d-block d-lg-none">
+                            <img className="first-image-width-small" src="/images/blobs/001 - Bob.png" height={"125%"} width={"125%"} />                        
+                        </div>                    
                     </div>
-                    <div className="col-2">
+                    <div className="col-2 d-none d-lg-block">
                         <div>
                             <img className="second-image-width" src="/images/YolgReverse.png" height={"125%"} width={"125%"} />                        
                         </div>
@@ -29,7 +34,8 @@ export const TitleContainer = () =>
             <style jsx>{`
                 .title-container {
                     background-image: linear-gradient(180deg, #143f6d, #1e5692); /*linear-gradient(180deg, #143f6d, #1e5692); linear-gradient(180deg, #2CBBE7, #2ea2be);*/
-                    min-height: 32rem;
+                    min-height: 20rem;
+                    height: 31vw;
 
                     color: white;
                     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
@@ -42,15 +48,35 @@ export const TitleContainer = () =>
                 */
 
                 h1 {
-                    font-size: 6.2vw;
+                    font-size: 6.0vw;
                     font-weight: 700;
                 }
 
                 p {
                     color: #ecf3fa;
-                    font-size: 1.2rem;
+                    font-size: 0.8rem;
                     font-weight: 500;
                 }
+
+                @media screen and (min-width: 996px) {
+                    h1 {
+                        font-size: 6.2vw;
+                        font-weight: 700;
+                    }
+
+
+                    p {
+                        font-size: 1.0rem;
+                    }                    
+                }
+
+                @media screen and (min-width: 1200px) {
+                    p {
+                        font-size: 1.2rem;
+                    }                    
+                }
+
+                
 
                 .position-first-blob {
                     position: absolute;
@@ -66,21 +92,33 @@ export const TitleContainer = () =>
                     position: relative;
                     width: 15vw;
                     max-width: 15vw;
-                    left: -10%;
-                    right: -10%;
-                    margin-left: -3vw;
-                    margin-right: -3vw;
+                    top: -2vw;
+                    left: -5%;
+                    right: -5%;
+                    margin-left: 0vw;
+                    margin-right: 0vw;
+                }
+
+                .first-image-width-small {
+                    position: relative;
+                    width: 25vw;
+                    max-width: 25vw;
+                    top: 2vw;
+                    left: 0%;
+                    right: 0%;
+                    margin-left: 0vw;
+                    margin-right: 0vw
                 }
 
                 .second-image-width {
                     position: relative;
                     width: 15vw;
                     max-width: 15vw;
-                    top: 10vw;
+                    top: 8vw;
                     left: -10%;
                     right: -10%;
-                    margin-left: -3vw;
-                    margin-right: -3vw;
+                    margin-left: 0vw;
+                    margin-right: 0vw;
                 }
             `}</style>  
         </div>
