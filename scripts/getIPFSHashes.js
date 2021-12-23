66e5364640a16a8851323df47c28b86f1e859113
Nicholas Maselli <maselli.nicholas@gmail.com>
2021-12-22 23:34:13 -0500

    cleaning keys

--- scripts/getIPFSHashes.js
+++ scripts/getIPFSHashes.js
@@ -1,24 +1,21 @@
 require('dotenv').config();
 
+// Using pinata to get ipfs data hashes
 const pinataSDK = require('@pinata/sdk');
 const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_KEY);
 console.log(process.env.PINATA_API_KEY);
 console.log(process.env.PINATA_SECRET_KEY);
 
 pinata.testAuthentication().then((result) => {
-    //handle results here
     console.log(result);
 }).catch((err) => {
-    //handle error here
     console.log(err);
 });
 
 pinata.pinList().then((result) => {
-    //handle results here
     console.log(result);
     console.log(result["rows"][0]["metadata"])
 
 }).catch((err) => {
-    //handle error here
     console.log(err);
 });
