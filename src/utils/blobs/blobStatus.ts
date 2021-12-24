1968499dd5dccc5c53e5f2bfad17bf0c85c45317
Nicholas Maselli <maselli.nicholas@gmail.com>
2021-12-23 20:40:09 -0500

    has auction started

--- src/utils/blobs/blobStatus.ts
+++ src/utils/blobs/blobStatus.ts
@@ -6,11 +6,11 @@ export const getBlobStatus = (blob : BlobChainAsset, blobOwnerData: any, blobScr
     if (blobOwnerData && blob.asset in blobOwnerData) {
         blobStatus = BlobStatus.Auction;
     }
+    else if (blobScriptData && blob.asset in blobScriptData && hasAuctionStarted(blobScriptData[blob.asset])) {
+        blobStatus = BlobStatus.Bid;
+    }
     else if (blobScriptData && blob.asset in blobScriptData && hasAuctionEnded(blobScriptData[blob.asset])) {
         blobStatus = BlobStatus.Close;
-    }
-    else if (blobScriptData && blob.asset in blobScriptData) {
-        blobStatus = BlobStatus.Bid;
     }
     else if (blobOwnerData && blobScriptData) {
         blobStatus = BlobStatus.Sold;
@@ -25,4 +25,13 @@ const hasAuctionEnded = (data: any) => {
 
     const ended = now > parseInt(endDatetime);
     return ended;
+}
+
+const hasAuctionStarted = (data: any) => {
+    const now = Date.now();
+    const startDatetime = data?.datum?.adAuctionDetails?.adStartTime;
+    if (!startDatetime) return false; 
+
+    const started = now > parseInt(startDatetime);
+    return started;
 }
