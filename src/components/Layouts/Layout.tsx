93fdc75c2fb770a386dbf049fcade432f9767382
Nicholas Maselli <maselli.nicholas@gmail.com>
2022-10-19 00:11:24 -0400

    ada blobs adds mercurychat support

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
+import { MercuryChat } from '@mercury-chat/react-chat';
 
 export const Layout = ({ children } : { children: React.ReactNode }) => 
 {
   return (
     <>
       <Navbar />
+      <MercuryChat position={'bottom-right'} hasFullscreen={false} showBackground={false} />
       <main>{children}</main>
       <Footer />
     </>
